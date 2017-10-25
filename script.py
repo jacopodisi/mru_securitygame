1072aa3f7efb3478f987b8b4b6041919e78bfbad
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-10-25 18:43:27 +0200

    [script] implemented compute_covering_routes function

--- script.py
+++ script.py
@@ -2,6 +2,8 @@
 
 import networkx as nx
 import numpy as np
+from scipy import sparse
+from srg import computecovsets as cs
 
 
 def compute_shortest_sets(matrix, deadlines):
@@ -25,3 +27,31 @@ def compute_shortest_sets(matrix, deadlines):
         cov = nx.single_source_shortest_path_length(G, tgt, cutoff=dl)
         shortest_matrix[cov.keys(), tgt] = 1
     return shortest_matrix
+
+
+def compute_covering_routes(graph_game, targets):
+    """ compute all the covering routes, from each vertex, for the given
+        set of targets.
+    Parameters
+    ----------
+    graph_game: instance of Graph class representing the game
+    targets: list of targets for which compute the covering routes
+
+    Return
+    ------
+    csr_matrices: dictionary of type {"vertex_number1": csr_matrix1,
+                                      "vertex_number2": csr_matrix2, ...}
+                  csr_matrix represent the covering sets of the vertex
+    """
+    num_vertices = len(graph_game.getVertices())
+    csr_matrices = {}
+    for v in xrange(num_vertices):
+        covset = cs.computeCovSet(graph_game, v, targets)
+        covset_matrix = np.zeros((len(covset), num_vertices), dtype=np.int8)
+        # cs_matrices[v] = np.vstack(route[0]
+        for route in xrange(len(covset)):
+            covset_matrix[route, covset[route][0]] = 1
+
+        csr_matrices[v] = sparse.csr_matrix(covset_matrix)
+    return csr_matrices
+
