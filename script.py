1abe1debf4bbc4c5a4d262a1a567b7ab168d72a4
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-10-29 10:58:20 +0100

    [script] modified vars type and names

--- script.py
+++ script.py
@@ -7,6 +7,9 @@ from srg import computecovsets as cs
 from srg import graph as gr
 
 
+mtype = np.uint8
+
+
 def compute_shortest_sets(graph_game, targets):
     """ Compute a list of array containing the reachable
         target from each vetrex.
@@ -26,7 +29,7 @@ def compute_shortest_sets(graph_game, targets):
         matrix[matrix == 999] = 0
     deadlines = {t: graph_game.getVertex(t).deadline for t in targets}
     G = nx.from_numpy_matrix(matrix)
-    shortest_matrix = np.zeros(shape=matrix.shape)
+    shortest_matrix = np.zeros(shape=matrix.shape, dtype=mtype)
     for tgt, dl in deadlines.iteritems():
         cov = nx.single_source_shortest_path_length(G, tgt, cutoff=dl)
         shortest_matrix[cov.keys(), tgt] = 1
@@ -47,13 +50,13 @@ def compute_covering_routes(graph_game, targets):
                                       "vertex_number2": csr_matrix2, ...}
                   csr_matrix represent the covering sets of the vertex
     """
-    num_vertices = len(graph_game.getVertices())
+    n_vertices = len(graph_game.getVertices())
     csr_matrices = {}
-    for v in xrange(num_vertices):
+    for v in range(n_vertices):
         covset = cs.computeCovSet(graph_game, v, targets)
-        covset_matrix = np.zeros((len(covset), num_vertices), dtype=np.int8)
+        covset_matrix = np.zeros((len(covset), n_vertices), dtype=mtype)
         # cs_matrices[v] = np.vstack(route[0]
-        for route in xrange(len(covset)):
+        for route in range(len(covset)):
             covset_matrix[route, covset[route][0]] = 1
 
         csr_matrices[v] = sparse.csr_matrix(covset_matrix)
