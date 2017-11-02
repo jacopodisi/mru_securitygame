2ca41332f087bcaa1c5e4590b473f3dcdcdfc1df
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-11-02 16:36:12 +0100

    [script] removed double main problem

--- script.py
+++ script.py
@@ -12,6 +12,55 @@ import set_cover as sc
 mtype = np.uint8
 
 
+def compute_value(graph, test=False, plot=False):
+    """ Compute the values of the graph for every number of resources
+        (from the minimum to the optimum)
+    Parameters
+    ----------
+    graph: instance of gr.Graph class
+
+    Return
+    ------
+    values: dictionary of type {"num_resources": game_value, ...}
+    """
+    if test:
+        print("Adjacency matrix:\n")
+        print(graph.getAdjacencyMatrix())
+
+    tgts = graph.getTargets()
+    # shortest matrix computation
+    shortest_matrix = compute_shortest_sets(graph, tgts)
+    csr = compute_covering_routes(graph, tgts)
+    success, min_resources = sc.set_cover_solver(shortest_matrix[:, tgts])
+    if not success:
+        return []
+    success, max_resources = sc.maximum_resources(csr, tgts)
+    if not success:
+        return []
+    if test:
+        print("\nShortest Matrix:\n")
+        print(shortest_matrix)
+        print("\nMinimum number of resources:\n")
+        print(min_resources)
+        print("\nMaximum number of resources:\n")
+        print(max_resources)
+    target_values = np.array([v.value for v in graph.vertices])
+    temp_dict = {k: csr[min_resources[k]] for k in range(len(min_resources))}
+    values = {}
+    if test:
+        print(temp_dict)
+    values[len(min_resources)], _, _ = cr.correlated(temp_dict, target_values)
+    for i in range(len(min_resources), len(max_resources) + 1):
+        success, res = sc.set_cover_solver(shortest_matrix[:, tgts], k=i)
+        temp_dict = {k: csr[res[k]] for k in range(len(res))}
+        values[i], _, _ = cr.correlated(temp_dict, target_values)
+    values[len(max_resources)] = 1
+    if test:
+        for key, val in values.iteritems():
+            print("Values of the game with {} res is {}".format(key, val))
+    return values
+
+
 def compute_shortest_sets(graph_game, targets):
     """ Compute a list of array containing the reachable
         target from each vetrex.
@@ -91,70 +140,6 @@ def compute_graph(file_path):
     vertices = np.array([])
     G = gr.Graph(vertices)
     return G
-
-
-if __name__ == '__main__':
-    mat = gr.generateRandMatrix(15, 0.2)
-    if not nx.is_connected(nx.from_numpy_matrix(mat)):
-        continue
-    graph = gr.generateRandomGraph(mat, np.shape(mat)[0], 0.8, 0, 3)
-    print("Adjacency matrix:\n")
-    print(graph.getAdjacencyMatrix())
-    print 'targets'
-    print graph.getTargets()
-    tgts = graph.getTargets()
-    # shortest matrix computation
-    shortest_matrix = compute_shortest_sets(graph, tgts)
-    csr = compute_covering_routes(graph, tgts)
-
-
-def compute_value(graph, test=False, plot=False):
-    """ Compute the values of the graph for every number of resources
-        (from the minimum to the optimum)
-    Parameters
-    ----------
-    graph: instance of gr.Graph class
-
-    Return
-    ------
-    values: dictionary of type {"num_resources": game_value, ...}
-    """
-    if test:
-        print("Adjacency matrix:\n")
-        print(graph.getAdjacencyMatrix())
-
-    tgts = graph.getTargets()
-    # shortest matrix computation
-    shortest_matrix = compute_shortest_sets(graph, tgts)
-    csr = compute_covering_routes(graph, tgts)
-    success, min_resources = sc.set_cover_solver(shortest_matrix[:, tgts])
-    if not success:
-        return []
-    success, max_resources = sc.maximum_resources(csr, tgts)
-    if not success:
-        return []
-    if test:
-        print("\nShortest Matrix:\n")
-        print(shortest_matrix)
-        print("\nMinimum number of resources:\n")
-        print(min_resources)
-        print("\nMaximum number of resources:\n")
-        print(max_resources)
-    target_values = np.array([v.value for v in graph.vertices])
-    temp_dict = {k: csr[min_resources[k]] for k in range(len(min_resources))}
-    values = {}
-    if test:
-        print(temp_dict)
-    values[len(min_resources)], _, _ = cr.correlated(temp_dict, target_values)
-    for i in range(len(min_resources), len(max_resources) + 1):
-        success, res = sc.set_cover_solver(shortest_matrix[:, tgts], k=i)
-        temp_dict = {k: csr[res[k]] for k in range(len(res))}
-        values[i], _, _ = cr.correlated(temp_dict, target_values)
-    values[len(max_resources)] = 1
-    if test:
-        for key, val in values.iteritems():
-            print("Values of the game with {} res is {}".format(key, val))
-    return values
 
 
 if __name__ == '__main__':
