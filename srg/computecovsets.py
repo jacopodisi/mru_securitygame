47d389ed151120d4c3a3a6c784d919f2b0eac1c7
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-11-04 13:45:53 +0100

    [computecovsets] modified the inputs to btree.update() in computeCovSet, using a purged binary vector

--- srg/computecovsets.py
+++ srg/computecovsets.py
@@ -67,6 +67,7 @@ def computeCovSet(G, v, targets):
                     if not U: #just take the depth of the tree where the nodes goes to the right(r contains the target)
                         C.append([np.append(q[0],w),cost]);
                         btree.update(C[-1][0],targets,btree.root,bt.binaryVectorFromRoute(C[-1][0],targets),C[-1][0]);#update the tree (maybe its better to do it in the search function?)
+                            btree.update(C[-1][0],targets,btree.root,bt.purgeBinaryVector(bt.binaryVectorFromRoute(C[-1][0],targets)),C[-1][0]);#update the tree (maybe its better to do it in the search function?)
         #print("\n\n",[c for c in C if len(c[0])==i+1]);
         if i > 0:
             C = purgeDominatedStrategies(C, i+1);
