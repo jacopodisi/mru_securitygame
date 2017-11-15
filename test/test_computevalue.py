69358397edb60567d535bb22b9e75fcae9d3dd01
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-11-15 17:08:22 +0100

    [test] added tests for the function compute_values

--- test/test_computevalue.py
+++ test/test_computevalue.py
@@ -88,6 +88,10 @@ class TestComputeValue1(unittest.TestCase):
         too_long = np.arange(len(self.G.getTargets()) + 1)
         self.assertRaises(ValueError, f, self.G, too_long)
 
+    def test_compute_values(self):
+        cv.compute_values(self.G)
+        self.assertTrue(True)
+
 
 class TestComputeValue2(unittest.TestCase):
     """
@@ -148,6 +152,10 @@ class TestComputeValue2(unittest.TestCase):
 
         too_long = np.arange(len(self.G.getTargets()) + 1)
         self.assertRaises(ValueError, f, self.G, too_long)
+
+    def test_compute_values(self):
+        cv.compute_values(self.G)
+        self.assertTrue(True)
 
 
 class TestComputeValue3(unittest.TestCase):
@@ -225,6 +233,10 @@ class TestComputeValue3(unittest.TestCase):
         too_long = np.arange(len(self.G.getTargets()) + 1)
         self.assertRaises(ValueError, f, self.G, too_long)
 
+    def test_compute_values(self):
+        cv.compute_values(self.G)
+        self.assertTrue(True)
+
 
 class TestComputeValue4(unittest.TestCase):
     """
@@ -295,6 +307,10 @@ class TestComputeValue4(unittest.TestCase):
 
         too_long = np.arange(len(self.G.getTargets()) + 1)
         self.assertRaises(ValueError, f, self.G, too_long)
+
+    def test_compute_values(self):
+        cv.compute_values(self.G)
+        self.assertTrue(True)
 
 
 class TestComputeValue5(unittest.TestCase):
@@ -376,3 +392,76 @@ class TestComputeValue5(unittest.TestCase):
 
         too_long = np.arange(len(self.G.getTargets()) + 1)
         self.assertRaises(ValueError, f, self.G, too_long)
+
+    def test_compute_values(self):
+        cv.compute_values(self.G)
+        self.assertTrue(True)
+
+
+class TestComputeValue6(unittest.TestCase):
+    """
+    Figure Triangle Graph  (1 SS, 2 MR)
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+        cls.v0 = gr.Vertex(1, 0.5, 1)
+        cls.v1 = gr.Vertex(1, 0.5, 1)
+        cls.v2 = gr.Vertex(1, 0.5, 1)
+
+        cls.G = gr.Graph(np.array([cls.v0, cls.v1, cls.v2]))
+        cls.G.setAdjacents(cls.v0, np.array([0, 1, 1]))
+        cls.G.setAdjacents(cls.v1, np.array([1, 0, 1]))
+        cls.G.setAdjacents(cls.v2, np.array([1, 1, 0]))
+
+    def test_compute_shortest_sets(self):
+        f = cv.compute_shortest_sets
+        result = f(self.G, self.G.getTargets())
+
+        shortest_sets = np.array(
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]])
+        ok = (result == shortest_sets).all()
+        ms = "Error computing the shortest sets\nResult:\n" \
+            + str(result) + "\nShould be:\n" + str(shortest_sets)
+        self.assertTrue(ok, msg=ms)
+
+        too_long = np.arange(len(self.G.getTargets()) + 1)
+        self.assertRaises(ValueError, f, self.G, too_long)
+
+    def test_compute_covering_routes(self):
+        f = cv.compute_covering_routes
+        cov_routes = f(self.G, self.G.getTargets())
+
+        result0 = cov_routes[self.v0.getVertexNumber()].toarray()
+        cov_route_0 = np.array(
+            [[1, 0, 0],
+             [1, 1, 0],
+             [1, 0, 1]])
+        ok0 = tf.compare_row_mat(result0, cov_route_0)
+        ms = "Error computing the covering routes v0\nResult:\n" \
+            + str(result0) + "\nShould be:\n" + str(cov_route_0)
+        self.assertTrue(ok0, msg=ms)
+
+        result2 = cov_routes[self.v2.getVertexNumber()].toarray()
+        cov_route_2 = np.array(
+            [[0, 0, 1],
+             [1, 0, 1],
+             [0, 1, 1]])
+        ok4 = tf.compare_row_mat(result2, cov_route_2)
+        ms = "Error computing the covering routes v4\nResult:\n" \
+            + str(result2) + "\nShould be:\n" + str(cov_route_2)
+        self.assertTrue(ok4, msg=ms)
+
+        too_long = np.arange(len(self.G.getTargets()) + 1)
+        self.assertRaises(ValueError, f, self.G, too_long)
+
+    def test_compute_values(self):
+        f = cv.compute_values
+        result = f(self.G)
+
+        values = {1: 0.75, 2: 1}
+
+        self.assertDictEqual(result, values)
