01ac5665e1efc7a25297c9c809dc89d58b569b2e
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-11-20 11:49:58 +0100

    [notebook_util] implemented some utility functions for ntoebooks

---
+++ notebook_utils.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+
+
+def print_srategy_for_n_player(
+        gr, n_res, result, cov_set, pos=None, color='r'):
+    num_nodes = nx.number_of_nodes(gr)
+    placements = result[1]
+    strategies = result[2]
+    n = n_res
+    strategy = np.zeros(shape=(len(strategies[n]), num_nodes))
+    prob_strategy = np.zeros(shape=(len(strategies[n])))
+    i = 0
+    string_routes = [''] * len(strategies[n])
+    for stra in strategies[n]:
+        for route_pl_id in stra[0]:
+            vert = placements[n][route_pl_id[0] - 1]
+            route = cov_set[vert][route_pl_id[1]].toarray()
+            strategy[i] = strategy[i] + route
+            r = str(np.where(route == 1)[1]) + '\n'
+            string_routes[i] = string_routes[i] + str(vert) + " --> " + r
+        prob_strategy[i] = stra[1]
+        i += 1
+    if pos is None:
+        pos = nx.spring_layout(gr)
+    for place in range(len(strategies[n])):
+        col = ['w'] * num_nodes
+        tr = np.where(strategy[place] > 0)[0]
+        for i in tr:
+            col[i] = color
+        plt.figure(place)
+        plt.title("probability " + str(prob_strategy[place]))
+        plt.figtext(1, 0.5, string_routes[place],
+                    verticalalignment='center')
+        nx.draw_networkx(gr, node_color=col, pos=pos)
+    plt.show()
+
+
+def print_dead_val(graph):
+    tgt = graph.getTargets()
+    deadlines = {t: graph.getVertex(t).deadline for t in tgt}
+    print "deadlines: " + str(deadlines)
+    node_values = {t: graph.getVertex(t).value for t in tgt}
+    print "values:"
+    for key, val in node_values.iteritems():
+        print str(key) + ": " + str(val)
+
+
+def compute_nx_graph(graph):
+    adj = graph.getAdjacencyMatrix()
+    adj[adj > 1] = 0
+    gr = nx.from_numpy_matrix(adj)
+    pos = nx.spring_layout(gr)
+    nx.draw_networkx(gr, node_color='y', pos=pos)
+    return gr, pos
+
+
+def plot_values(values):
+    val = sorted(values.items())
+    x, y = zip(*val)
+    plt.plot(x, y)
+    plt.show()
