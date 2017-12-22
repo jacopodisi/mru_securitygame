478a34b94452b5120bdc09c5b34b16f3b00e1f99
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-12-22 10:15:48 +0100

    executable mode to scripts

--- runners/run.sh
+++ runners/run.sh
@@ -3,3 +3,4 @@ python ../script.py -t 10 -d 25 -D 3 -i 0 -l logcore_1.log
 python ../script.py -t 10 -d 25 -D 3 -i 1 -l logcore_1.log
 python ../script.py -t 10 -d 25 -D 3 -i 2 -l logcore_1.log
 python ../script.py -t 10 -d 25 -D 3 -i 3 -l logcore_1.log
+python ../script.py -t 20 -d 25 -D 3 -i 0 -l logcore_1.log
