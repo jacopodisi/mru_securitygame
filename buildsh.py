e503fedb64a75b8e23110ecd7122beec8dac81aa
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-12-22 01:34:46 +0100

    [buildsh] build scripts in runners/ folder

--- buildsh.py
+++ buildsh.py
@@ -2,15 +2,15 @@ core = 1
 
 # write #!/bin/bash in scripts
 for c in range(1, 17):
-    fn = "runcore" + str(c) + ".sh"
+    fn = "./runners/runcore" + str(c) + ".sh"
     with open(fn, mode='a') as f:
         f.write("#!/bin/bash")
 
 # graph 20
 for d in [4, 5, 10, 15, 19]:
     for i in range(10):
-        fn = "runcore" + str(core) + ".sh"
-        command = "python script.py -d 25 -t 20 -D "\
+        fn = "./runners/runcore" + str(core) + ".sh"
+        command = "python ../script.py -d 25 -t 20 -D "\
                   + str(d) + " -i " + str(i) + " -l logcore"\
                   + str(core) + ".log"
         with open(fn, mode='a') as f:
@@ -23,8 +23,8 @@ for d in [4, 5, 10, 15, 19]:
 for t in [25, 30]:
     for d in [5, 10]:
         for i in range(10):
-            fn = "runcore" + str(core) + ".sh"
-            command = "python script.py -d 25 -t " + str(t) + " -D "\
+            fn = "./runners/runcore" + str(core) + ".sh"
+            command = "python ../script.py -d 25 -t " + str(t) + " -D "\
                       + str(d) + " -i " + str(i) + " -l logcore"\
                       + str(core) + ".log"
             with open(fn, mode='a') as f:
@@ -37,8 +37,8 @@ for t in [25, 30]:
 for t in [35, 40, 45, 50]:
     for d in [5, 10]:
         for i in [0, 1]:
-            fn = "runcore" + str(core) + ".sh"
-            command = "python script.py -d 25 -t " + str(t) + " -D "\
+            fn = "./runners/runcore" + str(core) + ".sh"
+            command = "python ../script.py -d 25 -t " + str(t) + " -D "\
                       + str(d) + " -i " + str(i) + " -l logcore"\
                       + str(core) + ".log"
             with open(fn, mode='a') as f:
