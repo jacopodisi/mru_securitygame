33ef5335a373a8c7c05f3fdd27832ac1755d259e
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-12-25 17:17:16 +0100

    [screen.sh] modified option

--- runners/screen.sh
+++ runners/screen.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-screen -dmS "core1" ./runcore1.sh
-screen -dmS "core2" ./runcore2.sh
-screen -dmS "core3" ./runcore3.sh
-screen -dmS "core4" ./runcore4.sh
-screen -dmS "core5" ./runcore5.sh
-screen -dmS "core6" ./runcore6.sh
-screen -dmS "core7" ./runcore7.sh
-screen -dmS "core8" ./runcore8.sh
-screen -dmS "core9" ./runcore9.sh
-screen -dmS "core10" ./runcore10.sh
-screen -dmS "core11" ./runcore11.sh
-screen -dmS "core12" ./runcore12.sh
-screen -dmS "core13" ./runcore13.sh
-screen -dmS "core14" ./runcore14.sh
-screen -dmS "core15" ./runcore15.sh
-screen -dmS "core16" ./runcore16.sh
+screen -d -S "core1"  -m ./runcore1.sh
+screen -d -S "core2"  -m ./runcore2.sh
+screen -d -S "core3"  -m ./runcore3.sh
+screen -d -S "core4"  -m ./runcore4.sh
+screen -d -S "core5"  -m ./runcore5.sh
+screen -d -S "core6"  -m ./runcore6.sh
+screen -d -S "core7"  -m ./runcore7.sh
+screen -d -S "core8"  -m ./runcore8.sh
+screen -d -S "core9"  -m ./runcore9.sh
+screen -d -S "core10" -m ./runcore10.sh
+screen -d -S "core11" -m ./runcore11.sh
+screen -d -S "core12" -m ./runcore12.sh
+screen -d -S "core13" -m ./runcore13.sh
+screen -d -S "core14" -m ./runcore14.sh
+screen -d -S "core15" -m ./runcore15.sh
+screen -d -S "core16" -m ./runcore16.sh
 
