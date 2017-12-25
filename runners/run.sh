98e502411e82a5c79ec933facbf63ea02c5996e8
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-12-25 17:19:34 +0100

    try with 10 nodes

--- runners/run.sh
+++ runners/run.sh
@@ -1,5 +1,5 @@
 
-python ../script.py -t 20 -d 10 -D 5 -i 0 -l logcore_1.log
+python ../script.py -t 10 -d 20 -D 4 -i 0 -l logcore_1.log
 # python ../script.py -t 10 -d 25 -D 3 -i 1 -l logcore_1.log
 # python ../script.py -t 10 -d 25 -D 3 -i 2 -l logcore_1.log
 # python ../script.py -t 10 -d 25 -D 3 -i 3 -l logcore_1.log
