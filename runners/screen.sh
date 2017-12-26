a8bdbd942e4ad87e911be217452724c24bce83c1
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-12-26 02:52:59 +0100

    source .profile in screen creation

--- runners/screen.sh
+++ runners/screen.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-screen -d -S "core1"  -m ./runcore1.sh
-screen -d -S "core2"  -m ./runcore2.sh
-screen -d -S "core3"  -m ./runcore3.sh
-screen -d -S "core4"  -m ./runcore4.sh
-screen -d -S "core5"  -m ./runcore5.sh
-screen -d -S "core6"  -m ./runcore6.sh
-screen -d -S "core7"  -m ./runcore7.sh
-screen -d -S "core8"  -m ./runcore8.sh
-screen -d -S "core9"  -m ./runcore9.sh
-screen -d -S "core10" -m ./runcore10.sh
-screen -d -S "core11" -m ./runcore11.sh
-screen -d -S "core12" -m ./runcore12.sh
-screen -d -S "core13" -m ./runcore13.sh
-screen -d -S "core14" -m ./runcore14.sh
-screen -d -S "core15" -m ./runcore15.sh
-screen -d -S "core16" -m ./runcore16.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+screen -dmS core1  bash -c 'source $HOME/.profile; $DIR/runcore1.sh'
+screen -dmS core2  bash -c 'source $HOME/.profile; $DIR/runcore2.sh'
+screen -dmS core3  bash -c 'source $HOME/.profile; $DIR/runcore3.sh'
+screen -dmS core4  bash -c 'source $HOME/.profile; $DIR/runcore4.sh'
+screen -dmS core5  bash -c 'source $HOME/.profile; $DIR/runcore5.sh'
+screen -dmS core6  bash -c 'source $HOME/.profile; $DIR/runcore6.sh'
+screen -dmS core7  bash -c 'source $HOME/.profile; $DIR/runcore7.sh'
+screen -dmS core8  bash -c 'source $HOME/.profile; $DIR/runcore8.sh'
+screen -dmS core9  bash -c 'source $HOME/.profile; $DIR/runcore9.sh'
+screen -dmS core10 bash -c 'source $HOME/.profile; $DIR/runcore10.sh'
+screen -dmS core11 bash -c 'source $HOME/.profile; $DIR/runcore11.sh'
+screen -dmS core12 bash -c 'source $HOME/.profile; $DIR/runcore12.sh'
+screen -dmS core13 bash -c 'source $HOME/.profile; $DIR/runcore13.sh'
+screen -dmS core14 bash -c 'source $HOME/.profile; $DIR/runcore14.sh'
+screen -dmS core15 bash -c 'source $HOME/.profile; $DIR/runcore15.sh'
+screen -dmS core16 bash -c 'source $HOME/.profile; $DIR/runcore16.sh'
 
