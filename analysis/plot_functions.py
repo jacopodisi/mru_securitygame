b0881063b2468b421c52c0fddbefa5a66be950cc
jacopodisi <disimone.jacopo@mail.polimi.it>
2018-01-29 17:49:22 +0100

    [plot_functions] plot margin and computational time without enumeration

--- analysis/plot_functions.py
+++ analysis/plot_functions.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def plot_meantimes(dead, den):
-    timesfile = "aggregate_times_1.pickle"
+    timesfile = "aggregate_times_0.pickle"
     with open(timesfile, mode='r') as f:
         timestuple = pickle.load(f)
 
@@ -21,14 +21,43 @@ def plot_meantimes(dead, den):
             meantime.append(np.mean(li))
     plt.plot(ntgts, meantime)
 
+    ax = plt.gca()
+    ax.grid(which='both')
+
     plt.xlabel('# of targets')
     plt.ylabel('mean computation time')
 
     plt.show()
 
 
+def plot_meantimesnoenum(dead, den):
+    timesfile = "aggregate_timesnoen_0.pickle"
+    with open(timesfile, mode='r') as f:
+        timestuple = pickle.load(f)
+
+    deadix = timestuple[1]
+    ntgtsix = timestuple[2]
+    denix = timestuple[3]
+    timesarr = timestuple[0]
+
+    meantime = []
+    ntgts = []
+    for ix, li in enumerate(timesarr[deadix[dead], :, denix[den]]):
+        if li is not None:
+            ntgts.append(ntgtsix.keys()[ntgtsix.values().index(ix)])
+            meantime.append(np.mean(li))
+    plt.plot(ntgts, meantime)
+    ax = plt.gca()
+    ax.grid(which='both')
+
+    plt.xlabel('# of targets')
+    plt.ylabel('computation time wihtout enumeration')
+
+    plt.show()
+
+
 def plot_maxres(dead, den):
-    maxresfile = "aggregate_maxres_1.pickle"
+    maxresfile = "aggregate_maxres_0.pickle"
     with open(maxresfile, mode='r') as f:
         maxrestuple = pickle.load(f)
 
@@ -44,6 +73,8 @@ def plot_maxres(dead, den):
             ntgts.append(ntgtsix.keys()[ntgtsix.values().index(ix)])
             maxres.append(np.mean(li))
     plt.plot(ntgts, maxres)
+    ax = plt.gca()
+    ax.grid(which='both')
 
     plt.xlabel('# of targets')
     plt.ylabel('optimal num of resources')
@@ -52,7 +83,7 @@ def plot_maxres(dead, den):
 
 
 def plot_gamevalue(dead, den, ntgts, ix):
-    gamevalsfile = "aggregate_gamevals_1.pickle"
+    gamevalsfile = "aggregate_gamevals_0.pickle"
     with open(gamevalsfile, mode='r') as f:
         gamevalstuple = pickle.load(f)
 
@@ -66,9 +97,39 @@ def plot_gamevalue(dead, den, ntgts, ix):
         x, y = zip(*val)
 
         plt.plot(x, y)
+        ax = plt.gca()
+        ax.grid(which='both')
 
         plt.xlabel('# of resources')
         plt.ylabel('game value')
+
+        plt.show()
+
+    else:
+        print 'results not yet computed'
+
+
+def plot_margin(dead, den, ntgts, ix, costres):
+    gamevalsfile = "aggregate_gamevals_0.pickle"
+    with open(gamevalsfile, mode='r') as f:
+        gamevalstuple = pickle.load(f)
+
+    deadix = gamevalstuple[1]
+    ntgtsix = gamevalstuple[2]
+    denix = gamevalstuple[3]
+    gamevalsarr = gamevalstuple[0]
+    vals = gamevalsarr[deadix[dead], ntgtsix[ntgts], denix[den]][ix]
+    if vals is not None:
+        marg = {k: i - k * costres for k, i in vals.iteritems()}
+        val = sorted(marg.items())
+        x, y = zip(*val)
+
+        plt.plot(x, y)
+        ax = plt.gca()
+        ax.grid(which='both')
+
+        plt.xlabel('# of resources')
+        plt.ylabel('margin')
 
         plt.show()
 
