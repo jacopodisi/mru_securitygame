f89adff46696e8fc9aa255d010477d4a377eec3b
jacopodisi <disimone.jacopo@mail.polimi.it>
2018-02-04 16:24:33 +0100

    script adaptation to choose the enumeration function that will be use

--- script.py
+++ script.py
@@ -24,14 +24,18 @@ from mru.patrolling.correlated import correlated_row_gen as cr
 def read_opt(str_opt):
     timeout = False
     logfile = den = ntgts = dead = ix = poolname = None
+    enumtype = "1"
+    enumit = "10"
     options, _ = getopt.getopt(str_opt,
-                               'd:t:D:i:l:p:T',
+                               'd:t:D:i:l:p:n:e:T',
                                ['density=',
                                 'ntarget=',
                                 'deadline=',
                                 'graphid=',
-                                'logfile='
+                                'logfile=',
                                 'poolname=',
+                                'enumiter=',
+                                'enumtype=',
                                 'timeout'])
 
     for opt, arg in options:
@@ -47,15 +51,17 @@ def read_opt(str_opt):
             logfile = arg
         elif opt in ('-p', '--poolname'):
             poolname = arg
+        elif opt in ('-n', '--enumiter'):
+            enumit = arg
+        elif opt in ('-e', '--enumtype'):
+            enumtype = arg
         elif opt in ('-T', '--timeout'):
             timeout = True
 
-    return den, ntgts, dead, ix, timeout, logfile, poolname
+    return den, ntgts, dead, ix, timeout, logfile, enumtype, enumit, poolname
 
 
-def main(den, ntgts, dead, ix, timeout, logfile):
-
-    enum = 10
+def main(den, ntgts, dead, ix, timeout, logfile, enumtype, enumit):
 
     if not (den and ntgts and dead and ix):
         print 'Value error: not defined every options of graph'
@@ -77,6 +83,8 @@ def main(den, ntgts, dead, ix, timeout, logfile):
 
     log.debug("START computation for graph " + ntgts + " " + dead +
               " 0." + den + " " + ix)
+    log.debug("options timeout=" + str(timeout) + " enumtype=" + enumtype +
+              " enumit=" + enumit)
 
     try:
         oldres = io.load_results(ntgts, dead, den, ix)
@@ -88,16 +96,18 @@ def main(den, ntgts, dead, ix, timeout, logfile):
     if timeout:
         with cr.time_limit(36000):
             result = cv.compute_values(graph,
-                                       rm_dominated=True,
-                                       enum=enum,
-                                       covset=covset)
+                                       rm_dom=True,
+                                       enum=enumit,
+                                       covset=covset,
+                                       enumtype=enumtype)
     else:
         result = cv.compute_values(graph,
-                                   rm_dominated=True,
-                                   enum=enum,
-                                   covset=covset)
+                                   rm_dom=True,
+                                   enum=enumit,
+                                   covset=covset,
+                                   enumtype=enumtype)
 
-    io.save_results(ntgts, dead, den, ix, result)
+    io.save_results(ntgts, dead, den, ix, result, enumtype)
 
     log.debug("END computation for graph " + ntgts + " " + dead +
               " 0." + den + " " + ix)
@@ -108,22 +118,28 @@ def main(den, ntgts, dead, ix, timeout, logfile):
 if __name__ == '__main__':
     path = os.path.dirname(os.path.realpath(__file__))
     options = read_opt(sys.argv[1:])
+    enumtp = options[6]
+    enumit = options[7]
 
+    # write in pidprocesses.txt the pid of the process
     with open(path + "/pidprocesses.txt", "a") as fin:
         fcntl.flock(fin, fcntl.LOCK_EX)
-        if options[-2] is not None:
-            pidstr = options[-2] + ' pid: ' + str(os.getpid())
+        if options[5] is not None:
+            pidstr = options[5] + ' pid: ' + str(os.getpid())
         else:
             pidstr = 'pid: ' + str(os.getpid())
         fin.write(pidstr + '\n')
         fcntl.flock(fin, fcntl.LOCK_UN)
 
     if len(sys.argv[1:]) >= 8:
+        # run the algorithm passing the option from terminal
         main(*options[:-1])
     else:
+        # run the algorithm pulling the GRAPHS option and timeout option
+        # from a pool file every other option is chose from terminal
         poolname = options[-1] if options[-1] is not None else path +\
             '/pool2.txt'
-        logfile = options[-2]
+        logfile = options[5]
         if not os.path.isfile(poolname):
             m = 'File {} do not exist'.format(poolname)
             raise IOError(m)
@@ -137,5 +153,5 @@ if __name__ == '__main__':
                 with open(poolname, "w") as fout:
                     fout.writelines(data[1:])
                 fcntl.flock(fin, fcntl.LOCK_UN)
-            options = read_opt(str_options)[:-2]
-            main(*(options + (logfile, )))
+            options = read_opt(str_options)[:-3]
+            main(*(options + (logfile, enumtp, enumit)))
