7d7b1731eed2994cfe69e5fd9099444d8dab12cb
jacopodisi <disimone.jacopo@mail.polimi.it>
2018-02-12 10:17:55 +0100

    avoid creation of new screen sessions

--- runners/run_pool.sh
+++ runners/run_pool.sh
@@ -1,61 +1,61 @@
 #!/bin/sh
-screen -dmS core01scr
+# screen -dmS core01scr
 screen -S core01scr -p 0 -X stuff 'python ../script.py -l logcorepool1.log\n'
-screen -dmS core02scr
+# screen -dmS core02scr
 screen -S core02scr -p 0 -X stuff 'python ../script.py -l logcorepool2.log\n'
-screen -dmS core03scr
+# screen -dmS core03scr
 screen -S core03scr -p 0 -X stuff 'python ../script.py -l logcorepool3.log\n'
-screen -dmS core04scr
+# screen -dmS core04scr
 screen -S core04scr -p 0 -X stuff 'python ../script.py -l logcorepool4.log\n'
-screen -dmS core05scr
+# screen -dmS core05scr
 screen -S core05scr -p 0 -X stuff 'python ../script.py -l logcorepool5.log\n'
-screen -dmS core06scr
+# screen -dmS core06scr
 screen -S core06scr -p 0 -X stuff 'python ../script.py -l logcorepool6.log\n'
-screen -dmS core07scr
+# screen -dmS core07scr
 screen -S core07scr -p 0 -X stuff 'python ../script.py -l logcorepool7.log\n'
-screen -dmS core08scr
+# screen -dmS core08scr
 screen -S core08scr -p 0 -X stuff 'python ../script.py -l logcorepool8.log\n'
-screen -dmS core09scr
+# screen -dmS core09scr
 screen -S core09scr -p 0 -X stuff 'python ../script.py -l logcorepool9.log\n'
-screen -dmS core10scr
+# screen -dmS core10scr
 screen -S core10scr -p 0 -X stuff 'python ../script.py -l logcorepool10.log\n'
-screen -dmS core11scr
+# screen -dmS core11scr
 screen -S core11scr -p 0 -X stuff 'python ../script.py -l logcorepool11.log\n'
-screen -dmS core12scr
+# screen -dmS core12scr
 screen -S core12scr -p 0 -X stuff 'python ../script.py -l logcorepool12.log\n'
-screen -dmS core13scr
+# screen -dmS core13scr
 screen -S core13scr -p 0 -X stuff 'python ../script.py -l logcorepool13.log\n'
-screen -dmS core14scr
+# screen -dmS core14scr
 screen -S core14scr -p 0 -X stuff 'python ../script.py -l logcorepool14.log\n'
-screen -dmS core15scr
+# screen -dmS core15scr
 screen -S core15scr -p 0 -X stuff 'python ../script.py -l logcorepool15.log\n'
-screen -dmS core16scr
+# screen -dmS core16scr
 screen -S core16scr -p 0 -X stuff 'python ../script.py -l logcorepool16.log\n'
-screen -dmS core17scr
+# screen -dmS core17scr
 screen -S core17scr -p 0 -X stuff 'python ../script.py -l logcorepool17.log\n'
-screen -dmS core18scr
+# screen -dmS core18scr
 screen -S core18scr -p 0 -X stuff 'python ../script.py -l logcorepool18.log\n'
-screen -dmS core19scr
+# screen -dmS core19scr
 screen -S core19scr -p 0 -X stuff 'python ../script.py -l logcorepool19.log\n'
-screen -dmS core20scr
+# screen -dmS core20scr
 screen -S core20scr -p 0 -X stuff 'python ../script.py -l logcorepool20.log\n'
-screen -dmS core21scr
+# screen -dmS core21scr
 screen -S core21scr -p 0 -X stuff 'python ../script.py -l logcorepool21.log\n'
-screen -dmS core22scr
+# screen -dmS core22scr
 screen -S core22scr -p 0 -X stuff 'python ../script.py -l logcorepool22.log\n'
-screen -dmS core23scr
+# screen -dmS core23scr
 screen -S core23scr -p 0 -X stuff 'python ../script.py -l logcorepool23.log\n'
-screen -dmS core24scr
+# screen -dmS core24scr
 screen -S core24scr -p 0 -X stuff 'python ../script.py -l logcorepool24.log\n'
-screen -dmS core25scr
+# screen -dmS core25scr
 screen -S core25scr -p 0 -X stuff 'python ../script.py -l logcorepool25.log\n'
-screen -dmS core26scr
+# screen -dmS core26scr
 screen -S core26scr -p 0 -X stuff 'python ../script.py -l logcorepool26.log\n'
- # screen -dmS core27scr
- # screen -S core27scr -p 0 -X stuff 'python ../script.py -l logcorepool27.log\n'
- # screen -dmS core28scr
- # screen -S core28scr -p 0 -X stuff 'python ../script.py -l logcorepool28.log\n'
- # screen -dmS core29scr
- # screen -S core29scr -p 0 -X stuff 'python ../script.py -l logcorepool29.log\n'
- # screen -dmS core30scr
- # screen -S core30scr -p 0 -X stuff 'python ../script.py -l logcorepool30.log\n'
+# screen -dmS core27scr
+# screen -S core27scr -p 0 -X stuff 'python ../script.py -l logcorepool27.log\n'
+# screen -dmS core28scr
+# screen -S core28scr -p 0 -X stuff 'python ../script.py -l logcorepool28.log\n'
+# screen -dmS core29scr
+# screen -S core29scr -p 0 -X stuff 'python ../script.py -l logcorepool29.log\n'
+# screen -dmS core30scr
+# screen -S core30scr -p 0 -X stuff 'python ../script.py -l logcorepool30.log\n'
