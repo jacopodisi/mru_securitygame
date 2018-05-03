91a1ec32e23ec1d97c20d4f749c0fc03a403707d
jacopodisi <disimone.jacopo@mail.polimi.it>
2018-05-03 12:05:11 +0200

    [test] implemented the test of multiple solutions of the ILP

--- mru/test/test_ilp_solver.py
+++ mru/test/test_ilp_solver.py
@@ -41,8 +41,9 @@ class TestILPSolver1(unittest.TestCase):
 
     def test_set_cover_solver_k_succ(self):
         sets = np.array(self.ok_sets)
-        result = ilp.set_cover_solver(sets, k=2)[0][0]
-        set_cover_k = np.array([3, 4])
+        result = ilp.set_cover_solver(sets, k=3, nsol=10)[0]
+        set_cover_k = np.array([[2, 3, 4], [0, 3, 4],
+                                [1, 3, 4], [1, 2, 3]])
         if set_cover_k.shape == result.shape:
             ok = (result == set_cover_k).all()
         else:
@@ -151,8 +152,8 @@ class TestILPSolver2(unittest.TestCase):
 
     def test_set_cover_solver_succ(self):
         sets = self.ok_sets
-        result = ilp.set_cover_solver(sets)[0][0]
-        set_cover_optimum = np.arange(9)
+        result = ilp.set_cover_solver(sets)[0]
+        set_cover_optimum = np.array([np.arange(9)])
         if set_cover_optimum.shape == result.shape:
             ok = (result == set_cover_optimum).all()
         else:
@@ -168,8 +169,8 @@ class TestILPSolver2(unittest.TestCase):
 
     def test_set_cover_solver_k_succ(self):
         sets = np.array(self.ok_sets)
-        result = ilp.set_cover_solver(sets, k=9)[0][0]
-        set_cover_k = np.arange(9)
+        result = ilp.set_cover_solver(sets, k=9)[0]
+        set_cover_k = np.array([np.arange(9)])
         if set_cover_k.shape == result.shape:
             ok = (result == set_cover_k).all()
         else:
@@ -197,6 +198,86 @@ class TestILPSolver2(unittest.TestCase):
                                      (13, 1),
                                      (13, 2),
                                      (13, 3)])
+        if len(result) == len(max_res_solution):
+            ok = np.all(np.in1d(result, max_res_solution))
+        else:
+            ok = False
+        self.assertTrue(
+            ok,
+            msg="Error computing the max resources 1:\n" + str(result))
+
+    def test_maximum_resources_fail1(self):
+        dict_sets = {
+            6: sparse.csr_matrix(self.wrong_sets[:5, :]),
+            13: sparse.csr_matrix(self.wrong_sets[5:, :])}
+        with self.assertRaises(gb.GurobiError):
+            ilp.maximum_resources(dict_sets, np.arange(9))
+
+    def test_maximum_resources_fail2(self):
+        dict_sets = {
+            6: sparse.csr_matrix(self.wrong_sets[:5, :]),
+            13: sparse.csr_matrix(self.wrong_sets[5:, :])}
+        with self.assertRaises(ValueError):
+            ilp.maximum_resources(dict_sets, np.arange(14))
+
+
+class TestILPSolver3(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.ok_sets = np.array(
+            # 0  1  2  3  4  5  6  7  8
+            [[1, 1, 1, 1, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 1, 1, 1, 1],
+             [1, 0, 1, 0, 1, 0, 1, 0, 1],
+             [0, 1, 0, 1, 0, 1, 0, 1, 0]])
+        cls.wrong_sets = np.array(
+            [[1, 1, 1, 1, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 1, 1, 1, 0],
+             [1, 0, 1, 0, 1, 0, 1, 0, 0],
+             [0, 1, 0, 1, 0, 1, 0, 1, 0]])
+
+    def test_set_cover_solver_succ(self):
+        sets = self.ok_sets
+        result = ilp.set_cover_solver(sets)[0]
+        set_cover_optimum = np.array([[2, 3]])
+        if set_cover_optimum.shape == result.shape:
+            ok = (result == set_cover_optimum).all()
+        else:
+            ok = False
+        self.assertTrue(
+            ok,
+            msg="Error computing the set cover optimum:\n" + str(result))
+
+    def test_set_cover_solver_fail(self):
+        sets = self.wrong_sets
+        with self.assertRaises(gb.GurobiError):
+            ilp.set_cover_solver(sets)
+
+    def test_set_cover_solver_k_succ(self):
+        sets = np.array(self.ok_sets)
+        result = ilp.set_cover_solver(sets, k=3, nsol=10)[0]
+        set_cover_k = np.array([[0, 1, 2], [0, 1, 3],
+                                [1, 2, 3], [0, 2, 3]])
+        if set_cover_k.shape == result.shape:
+            ok = (result == set_cover_k).all()
+        else:
+            ok = False
+        self.assertTrue(
+            ok,
+            msg="Error computing the set cover K:\n" + str(result))
+
+    def test_set_cover_solver_k_fail(self):
+        sets = np.array(self.ok_sets)
+        with self.assertRaises(gb.GurobiError):
+            ilp.set_cover_solver(sets, k=10)
+
+    def test_maximum_resources_success(self):
+        dict_sets = {
+            6: sparse.csr_matrix(self.ok_sets[:5, :]),
+            13: sparse.csr_matrix(self.ok_sets[5:, :])}
+        result = ilp.maximum_resources(dict_sets, np.arange(9))
+        max_res_solution = np.array([[6, 2], [6, 3]])
         if len(result) == len(max_res_solution):
             ok = np.all(np.in1d(result, max_res_solution))
         else:
